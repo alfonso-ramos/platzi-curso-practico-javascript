02c016c9ae5b4e880ac22c7b1c1f6b35a6bf8e23
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2021-10-16 13:02:09 -0600

    Reto completado de agreegacion de cupones en el ejercicio de precios y descuentos con funciones inteligentes

--- Precios y descuentos/descuentos.js
+++ Precios y descuentos/descuentos.js
@@ -8,17 +8,53 @@ function calcularPrecioConDescuento(precio, descuento){
     return precioFinalConDescuento;
 }
 
+// Implementacion de cupones
+
+// Lista de cupones
+
+const cupones = [
+    {
+        name: "Ponchido",
+        discount: 10,
+    },
+    {
+        name: "Descuento15",
+        discount: 15,
+    },
+    {
+        name:"Hola_Mundo",
+        discount: 100,
+    }
+]
+
+const inputCupon = document.getElementById("inputCupon");
+const cuponValue = inputCupon.value;
+
+
 function onClickButtonPriceDiscount(){
     let inputPrice = document.getElementById("inputPrice");
     let priceValue = inputPrice.value;
 
-    let inputDiscount = document.getElementById("inputDiscount");
-    let discountValue = inputDiscount.value;
+    let inputCupon = document.getElementById("inputCupon");
+    let cuponValue = inputCupon.value;
 
-    let precioFinalConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    const isCouponValueValid = function(coupon)
+    {
+        return coupon.name === cuponValue;
+    };
+    const userCupoun = cupones.find(isCouponValueValid);
+    if(!userCupoun){
+        alert("El cupon " + cuponValue + " no es valido");
+    } else {
+        const descuento = userCupoun.discount;
 
-    let resultPrice = document.getElementById("resultPrice");
-    resultPrice.innerText = "El precio final con descuento son $" + precioFinalConDescuento;
+        let precioFinalConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+        let resultPrice = document.getElementById("resultPrice");
+        resultPrice.innerText = "El precio final con descuento son $" + precioFinalConDescuento;
+    }
+
+
 
 }
 //El 100 representa solo el 100% de cualquier precio, no representa el precio
